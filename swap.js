06ccfcc1d1aae7a218b8e15754cb21898d8f429a
tarik-gadoumi <tarik.gadoumi@laplateforme.io>
2022-05-29 17:34:35 +0200

    simple swap

---
+++ swap.js
@@ -0,0 +1,8 @@
+var swap = function (array, firstIndex, secondIndex) {
+  var temp = array[firstIndex];
+  array[firstIndex] = array[secondIndex];
+  array[secondIndex] = temp;
+};
+
+var testArray = [7, 9, 4];
+swap(testArray, 0, 1);
