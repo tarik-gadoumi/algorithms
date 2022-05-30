03955e577bce7f8741881a4f442ade8ef6264e36
tarik-gadoumi <tarik.gadoumi@laplateforme.io>
2022-05-30 11:43:45 +0200

    adding 2nd way as comment

--- insert.js
+++ insert.js
@@ -1,9 +1,15 @@
 var insert = function (array, rightIndex, value) {
   var key = value;
+
   for (var i = rightIndex; key < array[i]; i--) {
     array[i + 1] = array[i];
   }
   array[i + 1] = key;
+
+  //   for (var j = rightIndex; j >= 0 && array[j] > value; j--) {
+  //     array[j + 1] = array[j];
+  //   }
+  //   array[j + 1] = value;
 };
 
 var array = [3, 5, 7, 11, 13, 2, 9, 6];
